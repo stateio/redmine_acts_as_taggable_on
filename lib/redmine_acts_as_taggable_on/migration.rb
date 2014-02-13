0227ece05eed5d4c88396d9a47595d5d66c88f7a
Phill MV <phillmv@okayfail.com>
2014-02-13 17:54:55 -0500

    Fixed migration template access.

--- lib/redmine_acts_as_taggable_on/migration.rb
+++ lib/redmine_acts_as_taggable_on/migration.rb
@@ -1,4 +1,37 @@
-require 'generators/acts_as_taggable_on/migration/templates/active_record/migration'
+# template is no longer accessible via LOAD_PATH, but it hasn't changed in two years, so:
+# subpar, I know, but it works for now.
+class ActsAsTaggableOnMigration < ActiveRecord::Migration
+  def self.up
+    create_table :tags do |t|
+      t.string :name
+    end
+
+    create_table :taggings do |t|
+      t.references :tag
+
+      # You should make sure that the column created is
+      # long enough to store the required class names.
+      t.references :taggable, :polymorphic => true
+      t.references :tagger, :polymorphic => true
+
+      # Limit is created to prevent MySQL error on index
+      # length for MyISAM table type: http://bit.ly/vgW2Ql
+      t.string :context, :limit => 128
+
+      t.datetime :created_at
+    end
+
+    add_index :taggings, :tag_id
+    add_index :taggings, [:taggable_id, :taggable_type, :context]
+  end
+
+  def self.down
+    drop_table :taggings
+    drop_table :tags
+  end
+end
+
+
 
 class RedmineActsAsTaggableOn::Migration < ActsAsTaggableOnMigration
   class SchemaMismatchError < StandardError; end
